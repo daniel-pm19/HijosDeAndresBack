1c60e97b3908c7b944802af4ffc4fdf0266794b2
Tulio Riano Sanchez <tulio.riano-s@mail.escuelaing.edu.co>
2025-11-04 14:39:04 -0500

    feat(core): Estructura Inicial

---
+++ src/main/java/edu/hackaton/HijosDeAndresBack/HijosDeAndresBackApplication.java
@@ -0,0 +1,13 @@
+package edu.hackaton.HijosDeAndresBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HijosDeAndresBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HijosDeAndresBackApplication.class, args);
+	}
+
+}
